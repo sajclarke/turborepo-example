f6a73e2e314cfd400f54e4fdc803e0806701b91f
sajclarke <shannonajclarke@gmail.com>
2023-02-08 09:48:41 -0400

    chore: modify landing page text

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,10 @@
 import { Button } from "ui";
 
 export default function Web() {
-  return (
-    <div>
-      <h1>Web</h1>
-      <Button />
-    </div>
-  );
+ return (
+  <div>
+   <h1>This is a test web app</h1>
+   <Button />
+  </div>
+ );
 }
